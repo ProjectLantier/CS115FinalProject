ff4ff427a05967e5c86f26578b85b94c0c44a1f7
Lauren130 <110861802+Lauren130@users.noreply.github.com>
2023-04-20 21:08:31 -0400

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -4,3 +4,19 @@
 import functools
 
 FILE = "musicrecplus.txt"
+
+
+filename = "musicrecplus.txt"
+
+def fileData(filename):
+    """ Converting text file into a dictionary """
+    dict = {}
+    with open(filename) as f:
+        if f == "":
+            return {}
+        for line in f:
+            (key, val) = line.split(":")
+            dict[key] = val
+    global dictGlobal
+    dictGlobal = dict
+    return dict
