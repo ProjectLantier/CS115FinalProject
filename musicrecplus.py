0c6d6f7b69d82552bb5f6ab48cf121e1cf3d99b1
Lauren130 <110861802+Lauren130@users.noreply.github.com>
2023-04-20 21:27:35 -0400

    q part, create saveQuit()

--- musicrecplus.py
+++ musicrecplus.py
@@ -8,7 +8,7 @@ FILE = "musicrecplus.txt"
 
 def fileData(filename):
     """ Converting text file into a dictionary """
-    #Lauren Kibalo
+    #Lauren
 
     dict = {}
     with open(filename) as f:
@@ -33,6 +33,7 @@ q - Save and quit""")
 
 def userChoice():
     """ Provides the user menu options """
+    #Lauren, Eshan, Bryce
 
     choices = input("Enter a letter to choose an option :\n e - Enter preferences\n r - Get recommendations\n p - Show most " \
            "popular artists\n h - How popular is the most popular\n m - Which user has the most likes\n d - Delete " \
@@ -50,11 +51,22 @@ def userChoice():
             #popMost()
         elif choices == "m":
             #mostLikes()
+        elif choices == "q":
+            saveQuit()
         else:
             choices = input("Enter a letter to choose an option :\n e - Enter preferences\n r - Get recommendations\n p - Show most " \
            "popular artists\n h - How popular is the most popular\n m - Which user has the most likes\n d - Delete " \
            "Preferences\n s - Show Preferences\n q - Save and " \
            "quit\n")
         
-        
+
+def saveQuit():
+    """ Saves the dictionary to the text file """
+    #Lauren
+
+    with open(FILE, 'w') as f:
+        for key in dictGlobal.keys():
+            f.write("%s:%s" % (key, dictGlobal[key]))
+    print("Preferences saved to", FILE, "Have a nice day!")
+    quit()
 
